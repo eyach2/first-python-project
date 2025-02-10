03cc59fffaa0e3ac90773043c2cfdff09dc3e333
eyach2 <149538273+eyach2@users.noreply.github.com>
2025-02-10 17:06:17 +0100

    Delete Translation-with-goslate.py

--- Translation-with-goslate.py
+++
@@ -1,5 +0,0 @@
-import goslate #importer la bibliothèque goslate de traduction du texte
-inserted_text="Be happy"
-new_gs=goslate.Goslate()
-translated_text=new_gs.translate(inserted_text,'tr') #traduire le texte entré dans la langue turque
-print(translated_text) #affichage du texte traduit 
